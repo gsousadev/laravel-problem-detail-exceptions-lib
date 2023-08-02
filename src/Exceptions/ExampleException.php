721279e4ac8062af981797e35c7b210cf1f75a35
gsousadev <guilherme.sousa@picpay.com>
2023-08-01 23:37:03 -0300

    fix: Ajustando Namespace

--- src/Exceptions/ExampleException.php
+++ src/Exceptions/ExampleException.php
@@ -2,9 +2,8 @@
 
 declare(strict_types=1);
 
-namespace Gsousadev\LaravelProblemDetailExceptions\Exceptions;
+namespace Exceptions;
 
-use Gsousadev\LaravelProblemDetailExceptions\Enums\InternalErrorCodeEnum;
 use Symfony\Component\HttpFoundation\Response;
 
 class ExampleException extends BaseException
